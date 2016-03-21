1d02d98bf7f297a858dae9ab3e0fc9cd25f29b8b
rackdon <martinez.calvo.ricardo@@gmail.com>
2016-03-21 18:43:28 +0100

    url checker added in Main Activity

--- app/src/main/java/app/rackdon/com/securitycam/MainActivity.java
+++ app/src/main/java/app/rackdon/com/securitycam/MainActivity.java
@@ -1,6 +1,10 @@
 package app.rackdon.com.securitycam;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,13 +18,38 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void videoActivity(View view) {
-        //TODO check if the url have been stablished
-        Intent intent = new Intent(this, StreamActivity.class);
-        startActivity(intent);
+        if (hasUrl()) {
+            startActivity(new Intent(this, StreamActivity.class));
+        }
+        else {
+            createUrlDialog(this).show();
+        }
     }
 
     public void configurationActivity(View view) {
-        Intent intent = new Intent(this, ConfigurationActivity.class);
-        startActivity(intent);
+        startActivity(new Intent(this, ConfigurationActivity.class));
+    }
+
+    public boolean hasUrl() {
+        String url = getSharedPreferences("SecurityCam", Context.MODE_PRIVATE).getString("Url", "");
+        return !url.equals("") ? true : false;
+    }
+
+    public AlertDialog.Builder createUrlDialog(final Context context) {
+        return new AlertDialog.Builder(context)
+                .setTitle("Url needed")
+                .setMessage("Please set the Url")
+                .setPositiveButton("Configuration", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        startActivity(new Intent(context, ConfigurationActivity.class));
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
     }
 }
